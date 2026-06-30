8352731384bd4001f221daa60137a53a51d56bde
Sheddy00 <123376307+Sheddy00@users.noreply.github.com>
2026-06-30 16:48:35 +0300

    v0.3.7

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # --- Configuration ---
-LAST_VERSION="0.3.6" # put here the latest version number
+LAST_VERSION="0.3.7" # put here the latest version number
 LINK='https://patrimoine.hei.school/latest' 
 
 JAR_NAME="patrimoine@${LAST_VERSION}.jar"
